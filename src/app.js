89d05fc24191d1be8388f010f712377fde074b91
vikashkumar6207 <vktd828403@gmail.com>
2024-06-03 12:39:34 +0530

    Don

--- src/app.js
+++ src/app.js
@@ -11,28 +11,32 @@ app.use(express.json());
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
 
+app.get("/api/v1/products/:id", (req, res) => {
+  const id = Number(req.params["id"]);
+  
 
-app.get("/api/v1//products/:id", (req, res)=>{
-  const userId = req.params.userId;
-  // const id = Number(req.params["id"]);
-
-  const product = products.find(item=> item.userId === userId);
-
-  if(!product){
-    res.statusCode = 404;
-    const payload = {"status": "failed", "message": "Product not found!"};
-    res.json(payload);
-  }else {
-    res.statusCode = 200;
-    const payload = {
-      status: "success", 
-      message: "Product fetched successfully",
-      data: {
-        product,
-      }, 
+  const product = products.find((product) => product.id === id);
+  console.log(product);
+  if (!product) {
+    req.status(404);
+    const output = {
+      status: "failed",
+      message: "Product not found!"
+      
     };
-    res.json(payload);
+    res.send(output);
+
+  } else {
+    req.statusCode(200);
+      const output = {
+        status: "success", 
+        message: "Product fetched successfully",
+        data: {
+          product,
+        },
   }
-})
+  res.send(output);
+}
+});
 
 module.exports = app;
